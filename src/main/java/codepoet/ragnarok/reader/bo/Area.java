6d61d86f732ad770ab54cf557222778c5e49545f
Sean Shookman <sms112788@gmail.com>
2017-08-11 21:59:43 -0500

    Adds sqlite annotations

--- src/main/java/codepoet/ragnarok/reader/bo/Area.java
+++ src/main/java/codepoet/ragnarok/reader/bo/Area.java
@@ -1,10 +1,18 @@
 package codepoet.ragnarok.reader.bo;
 
+import codepoet.vaultmonkey.annotations.SqliteColumn;
+import codepoet.vaultmonkey.annotations.SqliteObject;
+
+@SqliteObject(table = "area")
 public class Area {
 
+	@SqliteColumn
 	private Integer id;
+	@SqliteColumn
 	private Integer entityId;
+	@SqliteColumn
 	private String name;
+	@SqliteColumn
 	private String description;
 
 	public Integer getId() {
--- src/main/java/codepoet/ragnarok/reader/bo/Path.java
+++ src/main/java/codepoet/ragnarok/reader/bo/Path.java
@@ -1,11 +1,20 @@
 package codepoet.ragnarok.reader.bo;
 
+import codepoet.vaultmonkey.annotations.SqliteColumn;
+import codepoet.vaultmonkey.annotations.SqliteObject;
+
+@SqliteObject(table = "path")
 public class Path {
 
+	@SqliteColumn
 	private Integer id;
+	@SqliteColumn
 	private Integer entityId;
+	@SqliteColumn
 	private String name;
+	@SqliteColumn
 	private Integer areaAId;
+	@SqliteColumn
 	private Integer areaBId;
 
 	public Integer getId() {

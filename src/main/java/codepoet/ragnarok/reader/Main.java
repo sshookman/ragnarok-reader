808260e3174f755bfa902dfb58896e4d68c2bda8
Sean Shookman <sms112788@gmail.com>
2017-08-11 22:22:57 -0500

    Adds test game sqlite archive

--- src/main/java/codepoet/ragnarok/reader/Main.java
+++ src/main/java/codepoet/ragnarok/reader/Main.java
@@ -4,9 +4,9 @@ import codepoet.ragnarok.reader.display.ConsoleDisplay;
 
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, Exception {
 
-		Reader reader = new Reader(new ConsoleDisplay());
+		Reader reader = new Reader(new ConsoleDisplay(), "archives/TestGame.rag");
 		reader.read();
 	}
 }
--- src/main/java/codepoet/ragnarok/reader/Reader.java
+++ src/main/java/codepoet/ragnarok/reader/Reader.java
@@ -1,23 +1,32 @@
 package codepoet.ragnarok.reader;
 
+import codepoet.ragnarok.reader.bo.Area;
+import codepoet.ragnarok.reader.bo.Path;
 import codepoet.ragnarok.reader.display.DisplayColor;
 import codepoet.ragnarok.reader.display.DisplayInterface;
 import codepoet.ragnarok.reader.display.DisplaySpeed;
 import codepoet.ragnarok.reader.display.RichText;
+import codepoet.vaultmonkey.service.SqliteDataService;
+import codepoet.vaultmonkey.util.SqliteConnectionUtil;
+import java.sql.Connection;
 
 public class Reader {
 
+	private SqliteDataService<Area> areaDataService;
+	private SqliteDataService<Path> pathDataService;
+
 	private DisplayInterface display;
 
-	public Reader(final DisplayInterface display) {
+	public Reader(final DisplayInterface display, final String archive) throws Exception {
 		this.display = display;
+		Connection connection = SqliteConnectionUtil.establishConnection(archive);
+		this.areaDataService = new SqliteDataService<>(Area.class, connection);
+		this.pathDataService = new SqliteDataService<>(Path.class, connection);
 	}
 
 	public void read() {
 
 		RichText message = new RichText("Welcome to the Ragnarok Reader", DisplayColor.PURPLE, DisplaySpeed.SLOW);
-		display.write(message);
-		display.write("Welcome to the Ragnarok Reader!");
 		display.write(message);
 	}
 }

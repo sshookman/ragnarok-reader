b8695f28ed3cda6e79184e96b20eb9241839262b
Sean Shookman <sms112788@gmail.com>
2018-02-14 21:33:40 +0000

    Adds simple python nlp example for command parsing

--- commandParser.py
+++ commandParser.py
@@ -4,4 +4,17 @@ def getPosTags(command):
     command = nltk.tokenize.word_tokenize("I " + command)
     return nltk.pos_tag(command)
 
-print(getPosTags("walk to the door"))
+commands = [
+    "walk to the door",
+    "walk to door",
+    "run to the wall",
+    "crawl toward the cave",
+    "jump toward the window",
+    "head to the kitchen",
+    "mosey toward the tall, pale man",
+    "mosey toward the tall pale man",
+    "sprint at the blue goblin"
+ ]
+
+for command in commands:
+    print(getPosTags(command))

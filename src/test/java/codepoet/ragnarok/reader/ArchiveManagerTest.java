819e8a4a275a6391eee0e55f8e4b4016849dd9bc
Sean Shookman <sms112788@gmail.com>
2018-05-16 22:55:04 -0500

    Fixes ArchiveManagerTest

--- src/test/java/codepoet/ragnarok/reader/ArchiveManagerTest.java
+++ src/test/java/codepoet/ragnarok/reader/ArchiveManagerTest.java
@@ -7,8 +7,30 @@ import static org.junit.Assert.*;
 public class ArchiveManagerTest {
 
     @Test
-    public static void testInitializeArchive() throws Exception {
-        String gameFile = ArchiveManager.initializeArchive("test", "Ragnarok");
-        assertEquals("players/test/Ragnarok.rgn", gameFile);
+    public void testInitializeArchive() throws Exception {
+        String gameFile = ArchiveManager.initializeArchive("test", "Dragonfly");
+        assertEquals("players/test/Dragonfly.rgn", gameFile);
+    }
+
+    @Test
+    public void testInitializeArchiveNotFound() throws Exception {
+        try {
+            ArchiveManager.initializeArchive("test", "Dragonflies");
+            fail("Exception Expected");
+        } catch(Exception exception) {
+            assertNotNull(exception);
+            assertEquals("Unable to locate the archive", exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testInitializeArchiveCantCreateFolder() throws Exception {
+        try {
+            ArchiveManager.initializeArchive("test/test/test/test", "Dragonfly");
+            fail("Exception Expected");
+        } catch(Exception exception) {
+            assertNotNull(exception);
+            assertEquals("Unable to create player folder", exception.getMessage());
+        }
     }
 }
